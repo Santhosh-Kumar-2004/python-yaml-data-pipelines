e3583a3b9d2191f0930fabb94f247bb1d5405644
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-01-02 13:36:51 +0530

    Created the URL variable and added the DB url matching state and creating the engine based on ti

--- utils/db.py
+++ utils/db.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
 
 def get_engine(user, password, host, port, db):
-    pass
+    url = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}"
+    return create_engine(url)
