addca09b21377a3c1544ae65ccc3be85a69ce721
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-01-02 14:04:42 +0530

    Created the extract users fucntionn in the postgres extraction py file with creating db config vriable

--- src/extraact/postgres_extractor.py
+++ src/extraact/postgres_extractor.py
@@ -5,3 +5,16 @@ from utils.yaml_loader import load_yaml
 from utils.logger import get_logger
 
 logger = get_logger(__name__)
+
+def extract_users():
+    # Load DB config
+    db_config = load_yaml("configs/db.yaml")["source_db"]
+
+    # Create engine
+    engine = get_engine(
+        user=db_config["user"],
+        password=db_config["password"],
+        host=db_config["host"],
+        port=db_config["port"],
+        database=db_config["database"],
+    )
