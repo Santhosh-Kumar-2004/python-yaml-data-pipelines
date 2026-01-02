fe8dd1c43bdeae89798988e3a114e5e1694e74cf
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-01-02 14:01:04 +0530

    Importing aall the thinsg from the utils folder

--- src/extraact/postgres_extractor.py
+++ src/extraact/postgres_extractor.py
@@ -1,2 +1,5 @@
 # Ths file is only used for the Python - Postgres Extraction (Just etsting purpose)
 import pandas as pd
+from utils.db import get_engine
+from utils.yaml_loader import load_yaml
+from utils.logger import get_logger
