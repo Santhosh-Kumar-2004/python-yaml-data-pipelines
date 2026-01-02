49d52ed2a359f130dadb752848d20dc2442779d2
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-01-02 16:16:21 +0530

    Added the sql query which makes the email into lower and clears duplicates and also orders correctly

--- transform/sql/users_transform.sql
+++ transform/sql/users_transform.sql
@@ -7,3 +7,23 @@ SELECT
 FROM raw.users
 WHERE email IS NOT NULL;
 
+WITH ranked_users AS (
+    SELECT
+        user_id,
+        LOWER(email) AS email,
+        full_name,
+        created_at,
+        ROW_NUMBER() OVER (
+            PARTITION BY LOWER(email)
+            ORDER BY created_at DESC
+        ) AS rn
+    FROM raw.users
+    WHERE email IS NOT NULL
+)
+SELECT
+    user_id,
+    email,
+    full_name,
+    created_at
+FROM ranked_users
+WHERE rn = 1;
